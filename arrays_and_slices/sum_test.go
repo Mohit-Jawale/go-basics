4228698f884f0c03ae99272abe2ae3b2b9e9bd5f
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2025-02-04 12:09:02 -0800

    array & slices

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,17 +1,23 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers array", func(t *testing.T) {
 
-	got := Sum(numbers)
-	want := 15
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("got %d want %d given,%v", got, want, numbers)
+		got := Sum(numbers)
+		want := 15
 
-	}
+		if got != want {
+			t.Errorf("got %d want %d given,%v", got, want, numbers)
+
+		}
+
+	})
 
 }
