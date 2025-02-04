5dd655d0db9075f3d7828ffda923f5cf2281a8e3
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2025-02-04 13:26:08 -0800

    adding final refactor code for array and slices

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -13,3 +13,30 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// func sumAll(numbersToSum ...[]int) []int {
+
+// 	// lengthofNumbers := len(numbersToSum)
+// 	// sums := make([]int, lengthofNumbers)
+// 	var sums []int
+
+// 	for _, number := range numbersToSum {
+// 		sums = append(sums, Sum(number))
+// 	}
+
+// 	return sums
+
+// }
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -19,5 +20,36 @@ func TestSum(t *testing.T) {
 		}
 
 	})
+
+	t.Run("tail of arrays", func(t *testing.T) {
+
+		got := SumAllTails([]int{1, 2}, []int{1, 3, 5})
+
+		want := []int{2, 8}
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+}
+
+// func TestSumall(t *testing.T) {
+
+// 	got := sumAll([]int{1, 2}, []int{0, 8})
+
+// 	want := []int{3, 8}
+
+// 	assertCorrectMessage(t, got, want)
+
+// }
+
+func assertCorrectMessage(t testing.TB, got, want []int) {
+
+	t.Helper()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v,", got, want)
+
+	}
 
 }
