5948d9066d5df82f30fb7215671cfed2548ce92f
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2025-02-03 16:03:26 -0800

    adding const and subset testing

--- hello.go
+++ hello.go
@@ -6,6 +6,10 @@ const englishHelloPrefix = "Hello "
 
 func Hello(name string) string {
 
+	if name == "" {
+		name = "World"
+	}
+
 	return englishHelloPrefix + name
 }
 
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,31 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Mohit")
-	want := "Hello Mohit"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Mohit")
+		want := "Hello Mohit"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+
+		got := Hello("")
+		want := "Hello World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+
+		}
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // to tell the compiler to display error on the above lines not here
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
